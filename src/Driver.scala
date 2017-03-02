84e68c2b7396a09cf3a3f381821bdf1e16e8ab81
Daniel Gamboa <dtgamboa@cpp.edu>
2017-03-02 05:18:26 -0800

    Fixed all indentations/white space to be 4 spaces. Added .gitignore file to ignore *.class files

--- src/Driver.scala
+++ src/Driver.scala
@@ -5,34 +5,25 @@ import javax.imageio.ImageIO
 /*
 * Driver that runs the main application of the program in conjunction with all
 * other classes.
-*
 * */
 object Driver extends App{
 
-  override def main(args: Array[String]): Unit = {
-    if (args.length != 2){
-      throw new IllegalArgumentException("Must only contain two arguments.")
+    override def main(args: Array[String]): Unit = { //FIXME: overriding the main method is deprecated
+        
+        if (args.length != 2){
+            throw new IllegalArgumentException("Must only contain two arguments.")
+        }
+
+        val fileInput = args(0)
+        val fileOutput = args(1)
+
+        val originalMaze = ImageIO.read(new File(fileInput))
+
+        val maze = new Maze(originalMaze)
+
+
+        //Following code tests that the picture's dimensions is being read accurately
+        //printf("Photo size is %d x %d\n", photo1.getWidth, photo1.getHeight)
+
     }
-
-    val fileInput = args(0)
-    val fileOutput = args(1)
-
-    val originalMaze = ImageIO.read(new File(fileInput))
-
-    val maze = new Maze(originalMaze)
-
-
-    //Following code tests that the picture's dimensions is being read accurately
-    //printf("Photo size is %d x %d\n", photo1.getWidth, photo1.getHeight)
-
-
-
-
-
-
-
-
-
-
-  }
 }
--- src/Maze.scala
+++ src/Maze.scala
@@ -5,51 +5,52 @@ import javax.imageio.ImageIO
 
 class Maze (bufferedImage: BufferedImage) {
 
-  val mazeHeight = bufferedImage.getHeight
-  val mazeWidth = bufferedImage.getWidth
+    val mazeHeight = bufferedImage.getHeight
+    val mazeWidth = bufferedImage.getWidth
 
-  //Create 2d array of Strings using the dimensions of the maze.
-  var mazeArray = Array.ofDim[MazeNode](mazeHeight, mazeWidth)
+    //Create 2d array of Strings using the dimensions of the maze.
+    var mazeArray = Array.ofDim[MazeNode](mazeHeight, mazeWidth)
 
-  for (i <- 0 until mazeHeight){
-    for(j <- 0 until mazeWidth){
-      mazeArray(i)(j) = new MazeNode(NodeState.OPEN, i, j)
+    for (i <- 0 until mazeHeight) {
+        for(j <- 0 until mazeWidth) {
+            mazeArray(i)(j) = new MazeNode(NodeState.OPEN, i, j)
+        }
     }
-  }
 
-  //Decimal representations of Colors used in the maze image
-  val white = -1
-  val black = -16777216
-  val blue = -16776961
-  val red = -65536
+    //Decimal representations of Colors used in the maze image
+    val white = -1
+    val black = -16777216
+    val blue = -16776961
+    val red = -65536
 
-  //Goes through the BufferedImage and then populates the 2D array of strings to be representative of the working layer.
-  for (i <- 0 until mazeHeight) {
-    for (j <- 0 until mazeWidth) {
-      if (bufferedImage.getRGB(i, j) == black) {
-        mazeArray(i)(j).nodeState = NodeState.CLOSED
-      }
-      else if(bufferedImage.getRGB(i, j) == white){
-        mazeArray(i)(j).nodeState = NodeState.OPEN
-      }
-      else if(bufferedImage.getRGB(i, j) == blue){
-        mazeArray(i)(j).nodeState = NodeState.VISITED
-      }
-      else if(bufferedImage.getRGB(i, j) == red){
-        mazeArray(i)(j).nodeState = NodeState.VISITED
-      }
-      else{
-        throw new NoSuchElementException
-      }
+    //Goes through the BufferedImage and then populates the 2D array of strings to be representative of the working layer.
+  
+    for (i <- 0 until mazeHeight) {
+        for (j <- 0 until mazeWidth) {
+            if (bufferedImage.getRGB(i, j) == black) {
+                mazeArray(i)(j).nodeState = NodeState.CLOSED
+            }
+            else if(bufferedImage.getRGB(i, j) == white){
+                mazeArray(i)(j).nodeState = NodeState.OPEN
+            }
+            else if(bufferedImage.getRGB(i, j) == blue){
+                mazeArray(i)(j).nodeState = NodeState.VISITED
+            }
+            else if(bufferedImage.getRGB(i, j) == red){
+                mazeArray(i)(j).nodeState = NodeState.VISITED
+            }
+            else {
+                throw new NoSuchElementException
+            }
+        }
     }
-  }
 
-  for (i <- 0 until mazeHeight){
-    for(j <- 0 until mazeWidth){
-      print(mazeArray(j)(i))
+    for (i <- 0 until mazeHeight){
+        for(j <- 0 until mazeWidth){
+            print(mazeArray(j)(i))
+        }
+        println()
     }
-    println()
-  }
 }
 
 
